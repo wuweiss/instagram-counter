0f3d7c08bfc2ad1a2480dee22f1fbe2dbb41b012
wuweiss <wuweiss@protonmail.ch>
2021-03-12 16:42:35 +0100

    Add nix-shell to project

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nodejs-14_x
+    pkgs.nodePackages.nodemon
+
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+}
